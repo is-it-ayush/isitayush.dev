68867ab70391b1ff6799777e5166de52298d9b0a
Ayush Gupta <ayush.keshav2004@gmail.com>
2023-02-05 18:50:28 +0530

    chore: initial init. removed boilerplate

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import {Inter} from "@next/font/google";
+
+const font = Inter({subsets: ["latin"]});
+
+export default function Home() {
+    return (
+        <main>
+            <h1>Hello</h1>
+        </main>
+    );
+}
